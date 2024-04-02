d498d4c52b3ef3602e2641db2da708deb5b85514
Benedito Magalhães <bencaceres1799@gmail.com>
2024-04-02 20:26:50 -0300

    Ultimo commit antes de fazer o CRUD de usuários.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -145,25 +145,25 @@
 <body>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{url('/')}}">Inicio</a>
+            <a class="navbar-brand" href="{{url('/')}}"><strong>Inicio</strong></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
                 aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{url('usuario/listar')}}">Usuarios</a>
+                        <a class="nav-link active" aria-current="page" href="{{url('usuario/listar')}}"><strong>Usuarios</strong></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{url('curso/listar')}}">Cursos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page"
-                            href="{{url('atividade/listar')}}">Atividades</a>
+                        <a class="nav-link active" aria-current="page" href="{{url('curso/listar')}}"><strong>Cursos</strong></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page"
-                            href="{{url('about')}}">Regras</a>
+                            href="{{url('atividade/listar')}}"><strong>Atividades</strong></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page"
+                            href="{{url('about')}}"><strong>Regras</strong></a>
                     </li>
                     </li>
                 </ul>
